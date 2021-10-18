d23582def2793b38bdc0febb2710a569c8be2f56
Yurou <zhouyurou07@gmail.com>
2021-10-18 17:25:55 +0800

    edited adminresource

--- NUStartApplication/NUStartApplication-war/src/java/webservices/restful/AdminResource.java
+++ NUStartApplication/NUStartApplication-war/src/java/webservices/restful/AdminResource.java
@@ -11,6 +11,7 @@ import entity.Student;
 import error.NoResultException;
 import java.util.List;
 import entity.Category;
+import entity.Facility;
 import javax.ejb.EJB;
 import javax.json.Json;
 import javax.json.JsonObject;
@@ -42,64 +43,89 @@ public class AdminResource {
     @EJB
     private StudentSessionBeanLocal studentSessionBeanLocal;
 
-//    @GET
-//    @Produces(MediaType.APPLICATION_JSON)
-//    public List<Staff> getAllStaff() {
-//        return adminSessionBeanLocal.getAllStaff();
-//    }
-//    
-//    @GET
-//    @Produces(MediaType.APPLICATION_JSON)
-//    public List<Student> getAllStudent() {
-//        return adminSessionBeanLocal.getAllStudents();
-//    }
-    
-//    @GET
-//    @Produces(MediaType.APPLICATION_JSON)
-//    public List<Category> getAllCategories() {
-//        return adminSessionBeanLocal.getCategories();
-//    }
-//    
-//    @POST
-//    @Consumes(MediaType.APPLICATION_JSON)
-//    @Produces(MediaType.APPLICATION_JSON)
-//    public Guide createCategory(Category c) {
-//        adminSessionBeanLocal.addCategories(c);
-//        return c;
-//    }
-//    
-//    @PUT
-//    @Path("/{id}")
-//    @Consumes(MediaType.APPLICATION_JSON)
-//    @Produces(MediaType.APPLICATION_JSON)
-//    public Response editCategory(@PathParam("id") Long cId, Category c) {
-//        
-//        try {
-//            adminSessionBeanLocal.updateCat(c);
-//            return Response.status(204).build();
-//        } catch (Exception e) {
-//            JsonObject exception = Json.createObjectBuilder()
-//                    .add("error", "Not found")
-//                    .build();
-//            return Response.status(404).entity(exception)
-//                    .type(MediaType.APPLICATION_JSON).build();
-//        }
-//    }
-//
-//
-//    @DELETE
-//    @Path("/{cid}")
-//    @Produces(MediaType.APPLICATION_JSON)
-//    public Response deleteCategory(@PathParam("cid") String name) {
-//        try {
-//            adminSessionBeanLocal.deleteCategory(name);
-//            return Response.status(204).build();
-//        } catch (NoResultException e) {
-//            JsonObject exception = Json.createObjectBuilder()
-//                    .add("error", "Category not found")
-//                    .build();
-//            return Response.status(404).entity(exception).build();
-//        }
-//    }
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Staff> getAllStaff() {
+        return adminSessionBeanLocal.getAllStaff();
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Student> getAllStudent() {
+        return adminSessionBeanLocal.getAllStudents();
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Category> getAllCategories() {
+        return adminSessionBeanLocal.getCategories();
+    }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Category createCategory(Category c) {
+        adminSessionBeanLocal.addCategories(c);
+        return c;
+    }
+
+    @PUT
+    @Path("/{id}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response editCategory(@PathParam("id") Long cId, Category c) {
+
+        try {
+            adminSessionBeanLocal.updateCat(c);
+            return Response.status(204).build();
+        } catch (Exception e) {
+            JsonObject exception = Json.createObjectBuilder()
+                    .add("error", "Not found")
+                    .build();
+            return Response.status(404).entity(exception)
+                    .type(MediaType.APPLICATION_JSON).build();
+        }
+    }
+
+    @DELETE
+    @Path("/{cid}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response deleteCategory(@PathParam("cid") String name) {
+        try {
+            adminSessionBeanLocal.deleteCategory(name);
+            return Response.status(204).build();
+        } catch (NoResultException e) {
+            JsonObject exception = Json.createObjectBuilder()
+                    .add("error", "Category not found")
+                    .build();
+            return Response.status(404).entity(exception).build();
+        }
+    }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Facility createFacility(Facility f) {
+        adminSessionBeanLocal.addFacility(f);
+        return f;
+    }
+
+    @PUT
+    @Path("/{id}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response editFacility(@PathParam("id") Long fId, Facility f) {
+
+        try {
+            adminSessionBeanLocal.updateFacility(f);
+            return Response.status(204).build();
+        } catch (Exception e) {
+            JsonObject exception = Json.createObjectBuilder()
+                    .add("error", "Not found")
+                    .build();
+            return Response.status(404).entity(exception)
+                    .type(MediaType.APPLICATION_JSON).build();
+        }
+    }
 
 }
