aea91d4aee9c49e4987e92c5d034c2dc4901de74
Remus <remuskwan23@gmail.com>
2021-11-07 02:22:15 +0800

    Dependencies

--- ui/src/app/components/navBar/index.js
+++ ui/src/app/components/navBar/index.js
@@ -55,7 +55,7 @@ export default function NavBar({
                   <Link to="/">
                     <img
                       className="block h-8 w-auto"
-                      src="https://tailwindui.com/img/logos/workflow-mark.svg?color=indigo&shade=500"
+                      src="https://tailwindui.com/img/logos/workflow-mark.svg?color=rose&shade=500"
                       alt="Workflow"
                     />
                   </Link>
