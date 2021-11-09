f1e42b3d20991dcf104ee98fa907a6074a57dd05
Kim Deng <dengxq.kim@gmail.com>
2021-11-10 05:45:23 +0800

    Conditionally render registration fields based on position

--- ui/src/app/containers/RegisterPage/index.js
+++ ui/src/app/containers/RegisterPage/index.js
@@ -300,71 +300,77 @@ export default function RegisterPage() {
                             </div>
                         </Listbox>
 
-                        <Listbox value={year} onChange={setYear}>
-                            <div className="mt-1 relative">
-                                <Listbox.Label className="m-1 block text-sm font-medium text-gray-700">Year</Listbox.Label>
-                                <Listbox.Button className="relative w-full bg-white border border-gray-300 rounded-md shadow-sm pl-3 pr-10 py-2 text-left cursor-default focus:outline-none focus:ring-1 focus:ring-rose-500 focus:border-rose-500 sm:text-sm">
-                                    <span className="block truncate">{year.name}</span>
-                                    <span className="absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
-                                        <SelectorIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
-                                    </span>
-                                </Listbox.Button>
+                        {position.id === 1
+                            ? <div>
+                                <Listbox value={year} onChange={setYear}>
+                                    <div className="mt-1 relative">
+                                        <Listbox.Label className="m-1 block text-sm font-medium text-gray-700">Year</Listbox.Label>
+                                        <Listbox.Button className="relative w-full bg-white border border-gray-300 rounded-md shadow-sm pl-3 pr-10 py-2 text-left cursor-default focus:outline-none focus:ring-1 focus:ring-rose-500 focus:border-rose-500 sm:text-sm">
+                                            <span className="block truncate">{year.name}</span>
+                                            <span className="absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
+                                                <SelectorIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
+                                            </span>
+                                        </Listbox.Button>
 
-                                <Transition as={Fragment} leave="transition ease-in duration-100" leaveFrom="opacity-100" leaveTo="opacity-0">
-                                    <Listbox.Options className="absolute z-10 mt-1 w-full bg-white shadow-lg max-h-60 rounded-md py-1 text-base ring-1 ring-black ring-opacity-5 overflow-auto focus:outline-none sm:text-sm">
-                                        {years.map((y) => (
-                                            <Listbox.Option
-                                                key={y.id}
-                                                className={({ active }) =>
-                                                    classNames(
-                                                        active ? 'text-white bg-rose-600' : 'text-gray-900',
-                                                        'cursor-default select-none relative py-2 pl-8 pr-4'
-                                                    )
-                                                }
-                                                value={y}
-                                            >
-                                                {({ selected, active }) => (
-                                                    <>
-                                                        <span className={classNames(selected ? 'font-semibold' : 'font-normal', 'block truncate')}>
-                                                            {y.name}
-                                                        </span>
+                                        <Transition as={Fragment} leave="transition ease-in duration-100" leaveFrom="opacity-100" leaveTo="opacity-0">
+                                            <Listbox.Options className="absolute z-10 mt-1 w-full bg-white shadow-lg max-h-60 rounded-md py-1 text-base ring-1 ring-black ring-opacity-5 overflow-auto focus:outline-none sm:text-sm">
+                                                {years.map((y) => (
+                                                    <Listbox.Option
+                                                        key={y.id}
+                                                        className={({ active }) =>
+                                                            classNames(
+                                                                active ? 'text-white bg-rose-600' : 'text-gray-900',
+                                                                'cursor-default select-none relative py-2 pl-8 pr-4'
+                                                            )
+                                                        }
+                                                        value={y}
+                                                    >
+                                                        {({ selected, active }) => (
+                                                            <>
+                                                                <span className={classNames(selected ? 'font-semibold' : 'font-normal', 'block truncate')}>
+                                                                    {y.name}
+                                                                </span>
 
-                                                        {selected ? (
-                                                            <span
-                                                                className={classNames(
-                                                                    active ? 'text-white' : 'text-rose-600',
-                                                                    'absolute inset-y-0 left-0 flex items-center pl-1.5'
-                                                                )}
-                                                            >
-                                                                <CheckIcon className="h-5 w-5" aria-hidden="true" />
-                                                            </span>
-                                                        ) : null}
-                                                    </>
-                                                )}
-                                            </Listbox.Option>
-                                        ))}
-                                    </Listbox.Options>
-                                </Transition>
+                                                                {selected ? (
+                                                                    <span
+                                                                        className={classNames(
+                                                                            active ? 'text-white' : 'text-rose-600',
+                                                                            'absolute inset-y-0 left-0 flex items-center pl-1.5'
+                                                                        )}
+                                                                    >
+                                                                        <CheckIcon className="h-5 w-5" aria-hidden="true" />
+                                                                    </span>
+                                                                ) : null}
+                                                            </>
+                                                        )}
+                                                    </Listbox.Option>
+                                                ))}
+                                            </Listbox.Options>
+                                        </Transition>
+                                    </div>
+                                </Listbox>
+
+                                <input type="hidden" name="remember" defaultValue="true" />
+                                <div className="rounded-md shadow-sm -space-y-px">
+                                    <div>
+                                        <label htmlFor="course" className="sr-only">
+                                            Course
+                                        </label>
+                                        <input
+                                            id="course"
+                                            name="course"
+                                            required
+                                            className="appearance-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:ring-rose-500 focus:border-rose-500 focus:z-10 sm:text-sm"
+                                            placeholder="Course"
+                                            value={course}
+                                            onChange={(e) => setCourse(e.target.value)}
+                                        />
+                                    </div>
+                                </div>
                             </div>
-                        </Listbox>
+                            : null
+                        }
 
-                        <input type="hidden" name="remember" defaultValue="true" />
-                        <div className="rounded-md shadow-sm -space-y-px">
-                            <div>
-                                <label htmlFor="course" className="sr-only">
-                                    Course
-                                </label>
-                                <input
-                                    id="course"
-                                    name="course"
-                                    required
-                                    className="appearance-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:ring-rose-500 focus:border-rose-500 focus:z-10 sm:text-sm"
-                                    placeholder="Course"
-                                    value={course}
-                                    onChange={(e) => setCourse(e.target.value)}
-                                />
-                            </div>
-                        </div>
 
                         <input type="hidden" name="remember" defaultValue="true" />
                         <div className="rounded-md shadow-sm -space-y-px">
