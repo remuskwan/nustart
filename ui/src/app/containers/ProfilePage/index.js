8e8cbb67f747cd41387d21ca3bc14454bbc45e70
Kim Deng <dengxq.kim@gmail.com>
2021-11-11 19:06:06 +0800

    added password tab

--- ui/src/app/containers/ProfilePage/index.js
+++ ui/src/app/containers/ProfilePage/index.js
@@ -32,6 +32,7 @@ import ContactsTab from './contactsTab'
 import AccountTab from './accountTab';
 import ThreadsTab from './threadsTab'
 import PostsTab from './postsTab'
+import PasswordTab from './passwordTab'
 
 const tabs = [
     { name: 'Profile', href: '#', current: true },
@@ -252,62 +253,62 @@ export default function ProfilePage() {
         } else if (activeTab[0].name === 'Threads') {
             return <ThreadsTab threads={threads} />
         } else {
-            return <LikedTab editMode={editMode} />;
+            return <PasswordTab user={user} />
         }
     }
 
-    function LikedTab(editMode) {
-        return (
-            <div>
-                <Listbox value={selected} onChange={setSelected}>
-                    <div className="p-5 mt-1 relative">
-                        <Listbox.Button className="relative w-full bg-white border border-gray-300 rounded-md shadow-sm pl-3 pr-10 py-2 text-left cursor-default focus:outline-none focus:ring-1 focus:ring-rose-500 focus:border-rose-500 sm:text-sm">
-                            <span className="block truncate">{selected.name}</span>
-                            <span className="absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
-                                <SelectorIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
-                            </span>
-                        </Listbox.Button>
+    // function LikedTab(editMode) {
+    //     return (
+    //         <div>
+    //             <Listbox value={selected} onChange={setSelected}>
+    //                 <div className="p-5 mt-1 relative">
+    //                     <Listbox.Button className="relative w-full bg-white border border-gray-300 rounded-md shadow-sm pl-3 pr-10 py-2 text-left cursor-default focus:outline-none focus:ring-1 focus:ring-rose-500 focus:border-rose-500 sm:text-sm">
+    //                         <span className="block truncate">{selected.name}</span>
+    //                         <span className="absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
+    //                             <SelectorIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
+    //                         </span>
+    //                     </Listbox.Button>
 
-                        <Transition as={Fragment} leave="transition ease-in duration-100" leaveFrom="opacity-100" leaveTo="opacity-0">
-                            <Listbox.Options className="absolute z-10 mt-1 w-full bg-white shadow-lg max-h-60 rounded-md py-1 text-base ring-1 ring-black ring-opacity-5 overflow-auto focus:outline-none sm:text-sm">
-                                {likedFilter.map((filter) => (
-                                    <Listbox.Option
-                                        key={filter.id}
-                                        className={({ active }) =>
-                                            classNames(
-                                                active ? 'text-white bg-rose-600' : 'text-gray-900',
-                                                'cursor-default select-none relative py-2 pl-8 pr-4'
-                                            )
-                                        }
-                                        value={filter}
-                                    >
-                                        {({ selected, active }) => (
-                                            <>
-                                                <span className={classNames(selected ? 'font-semibold' : 'font-normal', 'block truncate')}>
-                                                    {filter.name}
-                                                </span>
-                                                {selected ? (
-                                                    <span
-                                                        className={classNames(
-                                                            active ? 'text-white' : 'text-rose-600',
-                                                            'absolute inset-y-0 left-0 flex items-center pl-1.5'
-                                                        )}
-                                                    >
-                                                        <CheckIcon className="h-5 w-5" aria-hidden="true" />
-                                                    </span>
-                                                ) : null}
-                                            </>
-                                        )}
-                                    </Listbox.Option>
-                                ))}
-                            </Listbox.Options>
-                        </Transition>
-                    </div>
-                </Listbox>
-                <FilterFavourites />
-            </div>
-        )
-    }
+    //                     <Transition as={Fragment} leave="transition ease-in duration-100" leaveFrom="opacity-100" leaveTo="opacity-0">
+    //                         <Listbox.Options className="absolute z-10 mt-1 w-full bg-white shadow-lg max-h-60 rounded-md py-1 text-base ring-1 ring-black ring-opacity-5 overflow-auto focus:outline-none sm:text-sm">
+    //                             {likedFilter.map((filter) => (
+    //                                 <Listbox.Option
+    //                                     key={filter.id}
+    //                                     className={({ active }) =>
+    //                                         classNames(
+    //                                             active ? 'text-white bg-rose-600' : 'text-gray-900',
+    //                                             'cursor-default select-none relative py-2 pl-8 pr-4'
+    //                                         )
+    //                                     }
+    //                                     value={filter}
+    //                                 >
+    //                                     {({ selected, active }) => (
+    //                                         <>
+    //                                             <span className={classNames(selected ? 'font-semibold' : 'font-normal', 'block truncate')}>
+    //                                                 {filter.name}
+    //                                             </span>
+    //                                             {selected ? (
+    //                                                 <span
+    //                                                     className={classNames(
+    //                                                         active ? 'text-white' : 'text-rose-600',
+    //                                                         'absolute inset-y-0 left-0 flex items-center pl-1.5'
+    //                                                     )}
+    //                                                 >
+    //                                                     <CheckIcon className="h-5 w-5" aria-hidden="true" />
+    //                                                 </span>
+    //                                             ) : null}
+    //                                         </>
+    //                                     )}
+    //                                 </Listbox.Option>
+    //                             ))}
+    //                         </Listbox.Options>
+    //                     </Transition>
+    //                 </div>
+    //             </Listbox>
+    //             <FilterFavourites />
+    //         </div>
+    //     )
+    // }
 
     function FilterFavourites() {
         let filtered = [];
