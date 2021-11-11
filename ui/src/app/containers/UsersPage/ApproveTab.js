dbdb02f0ecbb6a1f96578d564027ccf455089718
Yurou <zhouyurou07@gmail.com>
2021-11-11 19:17:14 +0800

    edited approvetab, so sorry still cannot login maybe u can try testing the result

--- ui/src/app/containers/UsersPage/ApproveTab.js
+++ ui/src/app/containers/UsersPage/ApproveTab.js
@@ -18,19 +18,13 @@ export default function ApproveTab() {
     function approve(user) {
         user.accountStatus = 'ACTIVE'
         api.editUser(user.id, user)
-            .then(() => {
-                api.getUsers()
-                    .then((response) => setAllUsers(response.data))
-            })
+            .then(api.setAllUsers(response.data))
     }
 
     function disapprove(user) {
         user.accountStatus = 'REJECTED'
         api.editUser(user.id, user)
-            .then(() => {
-                api.getUsers()
-                    .then((response) => setAllUsers(response.data))
-            })
+            .then(api.setAllUsers(response.data))
     }
 
     return (
@@ -60,32 +54,28 @@ export default function ApproveTab() {
                                     </div>
                                 </div>
                                 <div className="ml-5 flex items-center">
-                                    {user.accountStatus === "UNAPPROVED"
-                                        ?
-                                        <button
-                                            type="button"
-                                            className="inline-flex items-center px-3 py-2 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-md text-white bg-rose-500 hover:bg-rose-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-rose-500"
-                                            onClick={() => {
-                                                approve(user);
-                                            }}
-                                        >
-                                            <CheckCircleIcon className="-ml-0.5 mr-2 h-4 w-4" aria-hidden="true" />
-                                            Approve
-                                        </button>
-                                        :
-                                        <button
-                                            type="button"
-                                            className="inline-flex items-center px-3 py-2 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-md text-white bg-rose-500 hover:bg-rose-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-rose-500"
-                                            onClick={() => {
-                                                disapprove(user);
-                                            }}
-                                        >
-                                            <BanIcon className="mr-3 h-4 w-4" aria-hidden="true"
-                                            />
-                                            Reject
-                                        </button>
+                                    <button
+                                        type="button"
+                                        className="inline-flex items-center px-3 py-2 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-md text-white bg-rose-500 hover:bg-rose-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-rose-500"
+                                        onClick={() => {
+                                            approve(user);
+                                        }}
+                                    >
+                                        <CheckCircleIcon className="-ml-0.5 mr-2 h-4 w-4" aria-hidden="true" />
+                                        Approve
+                                    </button>
 
-                                    }
+                                    <button
+                                        type="button"
+                                        className="inline-flex items-center px-3 py-2 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-md text-white bg-rose-500 hover:bg-rose-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-rose-500"
+                                        onClick={() => {
+                                            disapprove(user);
+                                        }}
+                                    >
+                                        <BanIcon className="mr-3 h-4 w-4" aria-hidden="true"
+                                        />
+                                        Reject
+                                    </button>
                                 </div>
                             </div>
                         </li>
